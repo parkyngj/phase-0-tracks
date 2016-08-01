ccfce319ba80f1593cba0bc501903215fb7894da
Young Ju Park <parkyngj@gmail.com>
2016-07-31 19:27:43 -0700

    Add multiple employee processing to program

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,55 +1,68 @@
-puts "What is your name? (FirstName LastName)"
-name = gets.chomp
+employee_num = nil
 
-puts "How old are you?"
-age = gets.chomp.to_i
+puts "How many employees will be processed?"
+employee_num = gets.chomp.to_i
 
-puts "What year were you born? (YYYY)"
-birth_year = gets.chomp.to_i
+current_employee = 0
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-garlic_pref = gets.chomp
+until current_employee == employee_num
 
-puts "Would you like to enroll in the company's health insurance? (y/n)"
-hi_pref = gets.chomp
+  puts "What is your name? (FirstName LastName)"
+  name = gets.chomp
 
-correct_age = nil
+  puts "How old are you?"
+  age = gets.chomp.to_i
 
-if age == (2016 - birth_year)
-  correct_age = true
-else correct_age = false
-end
+  puts "What year were you born? (YYYY)"
+  birth_year = gets.chomp.to_i
 
-def to_bool(str)
-  result = nil
-  if str == "y"
-    result = true
-  elsif str == "n"
-    result = false
-  else puts "Please enter a valid y/n string."
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+  garlic_pref = gets.chomp
+
+  puts "Would you like to enroll in the company's health insurance? (y/n)"
+  hi_pref = gets.chomp
+
+  correct_age = nil
+
+  if age == (2016 - birth_year)
+    correct_age = true
+  else correct_age = false
   end
+
+  def to_bool(str)
+    result = nil
+    if str == "y"
+      result = true
+    elsif str == "n"
+      result = false
+    else puts "Please enter a valid y/n string."
+    end
+  end
+
+  wants_garlic = to_bool(garlic_pref)
+
+  wants_hi = to_bool(hi_pref)
+
+  is_vampire = "Results inconclusive."
+
+  if correct_age && (wants_garlic || wants_hi)
+    is_vampire = "Probably not a vampire."
+  end
+
+  if !correct_age && !(wants_garlic && wants_hi)
+    is_vampire = "Probably a vampire."
+  end
+
+  if !correct_age && !(wants_garlic || wants_hi)
+    is_vampire = "Almost certainly a vampire."
+  end
+
+  if name == "Drake Cula" || name == "Tu Fang"
+    is_vampire = "Definitely a vampire."
+  end
+
+  puts is_vampire
+
+  current_employee = current_employee + 1
+  
 end
-
-wants_garlic = to_bool(garlic_pref)
-
-wants_hi = to_bool(hi_pref)
-
-is_vampire = "Results inconclusive."
-
-if correct_age && (wants_garlic || wants_hi)
-  is_vampire = "Probably not a vampire."
-end
-
-if !correct_age && !(wants_garlic && wants_hi)
-  is_vampire = "Probably a vampire."
-end
-
-if !correct_age && !(wants_garlic || wants_hi)
-  is_vampire = "Almost certainly a vampire."
-end
-
-if name == "Drake Cula" || name == "Tu Fang"
-  is_vampire = "Definitely a vampire."
-end
-
-puts is_vampire
