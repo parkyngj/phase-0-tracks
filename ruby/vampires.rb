0e2852083df42234e263beae2c4b2fe972499b12
Young Ju Park <parkyngj@gmail.com>
2016-07-31 19:40:24 -0700

    Add allergy check to vampire detection program

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,6 +7,8 @@ current_employee = 0
 
 until current_employee == employee_num
 
+  is_vampire = "Results inconclusive."
+
   puts "What is your name? (FirstName LastName)"
   name = gets.chomp
 
@@ -43,8 +45,6 @@ until current_employee == employee_num
 
   wants_hi = to_bool(hi_pref)
 
-  is_vampire = "Results inconclusive."
-
   if correct_age && (wants_garlic || wants_hi)
     is_vampire = "Probably not a vampire."
   end
@@ -55,6 +55,19 @@ until current_employee == employee_num
 
   if !correct_age && !(wants_garlic || wants_hi)
     is_vampire = "Almost certainly a vampire."
+  end
+  
+  puts "Do you have any allergies? Name them one at a time, pressing enter after each allergy. After exhausting all your allergies, please type done."
+  allergy = gets.chomp
+  
+  while allergy != "done"
+    if allergy == "sunshine"
+      is_vampire = "Probably a vampire."
+      puts "Any other allergies?"
+      allergy = gets.chomp
+    else puts "Any other allergies?"
+      allergy = gets.chomp
+    end
   end
 
   if name == "Drake Cula" || name == "Tu Fang"
