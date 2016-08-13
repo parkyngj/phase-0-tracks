98c458d9f2e23b6e3863b5232404f9624c7ea724
Young Ju Park <parkyngj@gmail.com>
2016-08-13 15:28:30 -0700

    Add getter and setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -28,6 +28,36 @@ class Santa
     puts "That was a good #{cookie} cookie!"
   end
 
+  # R2. "getter methods"
+
+  # The method age should simply return @age.
+  def age
+    @age
+  end
+
+  # The method ethnicity should return @ethnicity.
+  def ethnicity
+    @ethnicity
+  end
+
+  # R3. "setter methods" - Add three attribute-changing methods to your Santa class.
+
+  # celebrate_birthday should age Santa by one year.
+  def celebrate_birthday
+    @age = @age + 1
+  end
+
+  # get_mad_at can take a reindeer's name as an argument, and move that reindeer in last place in the reindeer rankings.
+  def get_mad_at(reindr)
+    current_ranking = @reindeer_ranking.index(reindr)
+    @reindeer_ranking.insert(-1, @reindeer_ranking.delete_at(current_ranking))
+  end
+
+  # allow gender to be reassigned from outside the class definition
+  def change_gender(new_gend)
+    @gender = new_gend
+  end
+
 end # end Santa class definition
 
 # R0. Add a bit of code below your class declaration to check that you're able to initialize a Santa instance and call its methods.
@@ -44,3 +74,16 @@ example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer n
 example_genders.length.times do |i|
   santas << Santa.new(example_genders[i], example_ethnicities[i])
 end
+
+# R2. Checking if getter and setter methods work.
+
+st = Santa.new("f","Korean")
+p st.age
+p st.celebrate_birthday
+p st.age
+p st.ethnicity
+p st.instance_variable_get(:@reindeer_ranking)
+p st.get_mad_at("Dancer")
+p st.instance_variable_get(:@reindeer_ranking)
+p st.instance_variable_get(:@gender)
+p st.change_gender("agender")
