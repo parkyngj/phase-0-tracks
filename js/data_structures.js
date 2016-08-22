b1026480ba0f525d75e260dcb45c0893a086305c
Young Ju Park <parkyngj@gmail.com>
2016-08-22 11:39:52 -0700

    Add release 0 colors and horses array

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var color = ['blue', 'white', 'rainbow', 'pink'];
+var horses = ['Twilight Sparkle', 'Pinkie Pie', 'Rainbow Dash', 'Applejack'];
+
+color.push('maroon');
+horses.push('Rarity');
+
+console.log(color);
+console.log(horses);
